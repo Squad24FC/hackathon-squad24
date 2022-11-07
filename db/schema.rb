099093d2e73304932f08f25ea83431f67b2f89fb
Alex Morais <alexsandermorais@hotmail.com>
2022-11-07 18:12:58 -0300

    feat: create courses controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_202200) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_204037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,35 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_202200) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "lesson_type", null: false
+    t.string "author", null: false
+    t.string "source", null: false
+    t.string "duration", null: false
+    t.bigint "section_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["section_id"], name: "index_lessons_on_section_id"
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string "name"
+    t.bigint "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_sections_on_course_id"
+  end
+
+  create_table "user_tracks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_user_tracks_on_course_id"
+    t.index ["user_id"], name: "index_user_tracks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_202200) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lessons", "sections"
+  add_foreign_key "sections", "courses"
+  add_foreign_key "user_tracks", "courses"
+  add_foreign_key "user_tracks", "users"
 end
